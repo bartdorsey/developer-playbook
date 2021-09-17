cb0bea37d74e9b2251e8bb79b9b6267ef6c7859c
Bart Dorsey <bart@bartdorsey.com>
2021-09-17 10:43:54 -0500

    Check for empty string

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 get_username() {
   echo "Enter your git username"
   read GIT_USER
-  if [ -z GIT_USER ]; then
+  if [ GIT_USER -eq "" ]; then
      get_username
   fi
 }
@@ -11,7 +11,7 @@ get_username() {
 get_email() {
   echo "Enter your git email"
   read GIT_EMAIL
-  if [ -z GIT_EMAIL ]; then
+  if [ -z GIT_EMAIL -eq "" ]; then
      get_email
   fi
 }
