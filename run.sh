1114f1a19ce4f99cb19918c3861ece29f2b54e64
Bart Dorsey <bart@bartdorsey.com>
2021-09-17 10:38:41 -0500

    Adding prompts for username and email

--- run.sh
+++ run.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
-GIT_USER=$1
-GIT_EMAIL=$2
+
+get_username() {
+  echo "Enter your git username"
+  read GIT_USER
+  if [ -z GIT_USER ]; then
+     get_username
+  fi
+}
+
+get_email() {
+  echo "Enter your git email"
+  read GIT_EMAIL
+  if [ -z GIT_EMAIL ]; then
+     get_email
+  fi
+}
+
+get_username
+get_email
 
 ansible-pull -U https://github.com/bartdorsey/developer-playbook.git -C multipass-test -e git_user="$GIT_USER" -e git_email="$GIT_EMAIL" playbook.yml
