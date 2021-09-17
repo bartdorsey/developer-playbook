3bffb94fd16153e35d0118d8080d3dbab8766840
Bart Dorsey <bart@bartdorsey.com>
2021-09-17 10:43:29 -0500

    Adding input verification step.

--- run.sh
+++ run.sh
@@ -16,7 +16,20 @@ get_email() {
   fi
 }
 
+verify_input() {
+  echo "Your Username is $GIT_USER";
+  echo "Your Email is $GIT_EMAIL";
+  echo "Is this correct (Y/N)";
+  read CORRECT
+  if [ CORRECT -eq 'N' ]; then
+    get_username
+    get_password
+    verify_input
+  fi
+}
+
 get_username
 get_email
+verify_input
 
 ansible-pull -U https://github.com/bartdorsey/developer-playbook.git -C multipass-test -e git_user="$GIT_USER" -e git_email="$GIT_EMAIL" playbook.yml
