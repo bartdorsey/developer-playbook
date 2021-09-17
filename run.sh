9911f5c99e14a41eb1a805401dd1a40717e185a7
Bart Dorsey <bart@bartdorsey.com>
2021-09-17 14:30:20 -0500

    Removing collections and adding ansible-galaxy install commands.

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+ansible-galaxy collection install community.postgresql
+ansible-galaxy collection install community.general
 ansible-pull -U https://github.com/bartdorsey/developer-playbook.git -K playbook.yml
