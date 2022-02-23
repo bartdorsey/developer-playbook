b9d83f67a357a6264cc689f935e0c26e9e8748a3
Bart Dorsey <bart@bartdorsey.com>
2022-02-23 20:15:45 +0000

    Making messages a little more friendly.

--- wsl.sh
+++ wsl.sh
@@ -5,7 +5,9 @@ sudo apt install -y software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible
 ansible-galaxy collection install community.postgresql community.general
-echo "Enter your sudo password again:"
+echo ""
+echo "Enter your sudo password again when prompted by BECOMES:"
+echo ""
 ansible-pull -K -U https://github.com/fullstackacademy/developer-playbook.git playbook.yml
 echo ""
 echo "Ansible setup complete, please close your Ubuntu Terminal and open a new one."
