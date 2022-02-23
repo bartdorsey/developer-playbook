b29c644012ff5921f9d0f5fabbe5e2595c07b7e4
Bart Dorsey <bart@bartdorsey.com>
2022-02-23 20:04:03 +0000

    Updating README, removing old run.sh script

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-ansible-galaxy collection install community.postgresql
-ansible-galaxy collection install community.general
-ansible-pull -U https://github.com/bartdorsey/developer-playbook.git -K playbook.yml
